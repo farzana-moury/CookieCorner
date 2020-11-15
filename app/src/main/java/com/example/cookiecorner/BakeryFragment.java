dc89d59a39858d391c4bffe24c44d7598bfc4b1c
= <=>
2020-11-14 19:02:44 -0500

    #2 Added Functionality for buttons within Bakery Fragment   * Gallery button takes you to the GalleryFragment   * Ratings button takes you to the RatingsFragment   * FAQ button takes you to the FAQFragment

--- app/src/main/java/com/example/cookiecorner/BakeryFragment.java
+++ app/src/main/java/com/example/cookiecorner/BakeryFragment.java
@@ -3,10 +3,12 @@ package com.example.cookiecorner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,35 @@ public class BakeryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bakery, container, false);
+        final View view = inflater.inflate(R.layout.fragment_bakery, container, false);
+
+        Button galleryButton = view.findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_nav_bakery_to_galleryFragment);
+            }
+        });
+
+        Button ratingsButton = view.findViewById(R.id.ratingsButton);
+        ratingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_nav_bakery_to_ratingsFragment);
+            }
+        });
+
+        Button faqButton = view.findViewById(R.id.faqButton);
+        faqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_nav_bakery_to_FAQFragment);
+            }
+        });
+
+        return view;
     }
 }
