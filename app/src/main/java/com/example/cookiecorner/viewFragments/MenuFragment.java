5afeec78f054ffd399de303d1afe71ef1536a2f8
Farzana Moury <w0737626@myscc.ca>
2020-12-02 19:20:32 -0500

    #5 Using the ShoppingCart menu   * provided code so MenuFragment Class accesses the menu   * from the ShoppingCart class to populate the Menu ListView with

--- app/src/main/java/com/example/cookiecorner/viewFragments/MenuFragment.java
+++ app/src/main/java/com/example/cookiecorner/viewFragments/MenuFragment.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.cookiecorner.R;
+import com.example.cookiecorner.ShoppingCart;
 import com.example.cookiecorner.pojo.CookieMenuItem;
 
 import java.util.ArrayList;
@@ -82,18 +83,18 @@ public class MenuFragment extends Fragment {
 
         final ListView listView = view.findViewById(R.id.menuList);
 
-        final ArrayList<CookieMenuItem> menu = new ArrayList<>();
-
-        menu.add(new CookieMenuItem("cookie1", 1.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie2", 1.50,  R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie3", 1.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie4", 2.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie5", 2.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie6", 2.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie7", 3.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie8", 3.25, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie9", 3.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie10", 2.75, R.drawable.logo, "cookie description goes here"));
+        final ArrayList<CookieMenuItem> menu = ShoppingCart.menu;
+//
+//        menu.add(new CookieMenuItem("cookie1", 1.00, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie2", 1.50,  R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie3", 1.00, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie4", 2.00, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie5", 2.50, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie6", 2.50, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie7", 3.00, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie8", 3.25, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie9", 3.50, R.drawable.logo, "cookie description goes here"));
+//        menu.add(new CookieMenuItem("cookie10", 2.75, R.drawable.logo, "cookie description goes here"));
 
         listView.setAdapter(new CustomListViewAdapter(getContext(), menu));
 
