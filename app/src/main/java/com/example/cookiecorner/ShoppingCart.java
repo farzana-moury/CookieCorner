ab2acd4e50a2036c00326b79b621f63ca4f5dbf0
Farzana Moury <w0737626@myscc.ca>
2020-12-08 12:17:02 -0500

    #6 Adding images for Menu   * added menu images for the rest of the items

--- app/src/main/java/com/example/cookiecorner/ShoppingCart.java
+++ app/src/main/java/com/example/cookiecorner/ShoppingCart.java
@@ -12,15 +12,15 @@ public class ShoppingCart {
     //ShoppingCart.menu will contain every cookie carried by the app
     static {
         menu.add(new CookieMenuItem("Strawberry Dream", 1.00, R.drawable.menu_cookie1, "A delicate cookie sandwich made with love. Perfect for Valentine's day!"));
-        menu.add(new CookieMenuItem("cookie2", 1.50,  R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie3", 1.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie4", 2.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie5", 2.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie6", 2.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie7", 3.00, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie8", 3.25, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie9", 3.50, R.drawable.logo, "cookie description goes here"));
-        menu.add(new CookieMenuItem("cookie10", 2.75, R.drawable.logo, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Peanut Fudge", 1.50,  R.drawable.menu_cookie2, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Sugary Shortbread", 1.00, R.drawable.menu_cookie3, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Vegan Cookie", 2.00, R.drawable.menu_cookie4, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Jolly Jelly", 2.50, R.drawable.menu_cookie5, "cookie description goes here"));
+        menu.add(new CookieMenuItem("GingerBread Cookie", 2.50, R.drawable.menu_cookie6, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Berry Blast", 3.00, R.drawable.menu_cookie7, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Chocolate Drizzle", 3.25, R.drawable.menu_cookie8, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Crunchy Munch", 3.50, R.drawable.menu_cookie9, "cookie description goes here"));
+        menu.add(new CookieMenuItem("Classic Cookie", 2.75, R.drawable.menu_cookie10, "cookie description goes here"));
     }
 
     private ShoppingCart(){
